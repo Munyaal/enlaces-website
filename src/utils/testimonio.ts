28d9e8389c6ff81af2ec8e7e507bfe30021edef3
Tania Cristel Perez Gonzalez <taniagonzalez1230@gmail.com>
2025-07-07 15:22:39 -0500

    Add page home, contact and about us

---
+++ src/utils/testimonio.ts
@@ -0,0 +1,28 @@
+export const testimonios = [
+  {
+    name: "Juan Alfonzo Perez Gonzalez",
+    message: "Un artículo mensual que resalta los avances en la industria de las telecomunicaciones y cómo impactan a los usuarios.",
+    estrellas: 5,
+    foto:"/images/user-1.webp",
+  },
+  {
+    name: "Juan Alfonzo Perez Gonzalez",
+    message: "Un artículo mensual que resalta los avances en la industria de las telecomunicaciones y cómo impactan a los usuarios.",
+    estrellas: 5,
+    foto:"/images/user-2.webp",
+  },
+  {
+    name: "Juan Alfonzo Perez Gonzalez",
+    message: "Un artículo mensual que resalta los avances en la industria de las telecomunicaciones y cómo impactan a los usuarios.",
+    estrellas: 5,
+    foto:"/images/user-3.webp",
+  },
+  {
+    name: "Juan Alfonzo Perez Gonzalez",
+    message: "Un artículo mensual que resalta los avances en la industria de las telecomunicaciones y cómo impactan a los usuarios.",
+    estrellas: 5,
+    foto:"/images/user-4.webp",
+  },
+  
+  
+];
