967b31cb45544f3ba3e4ea294528934715012113
TaniaGlz34 <taniagonzalez1230@gmail.com>
2025-07-23 14:56:35 -0500

    Revert "Add pages zone"

--- src/utils/testimonio.ts
+++ src/utils/testimonio.ts
@@ -1,27 +1,27 @@
 export const testimonios = [
   {
     name: "Juan Alfonzo Perez Gonzalez",
-    message: "Internet sin límite de descarga para tu hogar o negocio con soluciones a tu medida, equipo altamente capacitado y atención personalizada de Lunes a Sábado.",
+    message: "Un artículo mensual que resalta los avances en la industria de las telecomunicaciones y cómo impactan a los usuarios.",
+    estrellas: 5,
     foto:"/images/user-1.webp",
-    rol: "Cio Refacccionaria monterrey",
   },
   {
     name: "Juan Alfonzo Perez Gonzalez",
-    message: "Internet sin límite de descarga para tu hogar o negocio con soluciones a tu medida, equipo altamente capacitado y atención personalizada de Lunes a Sábado.",
+    message: "Un artículo mensual que resalta los avances en la industria de las telecomunicaciones y cómo impactan a los usuarios.",
+    estrellas: 5,
     foto:"/images/user-2.webp",
-    rol: "Empresario local",
   },
   {
     name: "Juan Alfonzo Perez Gonzalez",
-    message: "Internet sin límite de descarga para tu hogar o negocio con soluciones a tu medida, equipo altamente capacitado y atención personalizada de Lunes a Sábado.",
+    message: "Un artículo mensual que resalta los avances en la industria de las telecomunicaciones y cómo impactan a los usuarios.",
+    estrellas: 5,
     foto:"/images/user-3.webp",
-    rol: "Empresario local",
   },
   {
     name: "Juan Alfonzo Perez Gonzalez",
-    message: "Internet sin límite de descarga para tu hogar o negocio con soluciones a tu medida, equipo altamente capacitado y atención personalizada de Lunes a Sábado.",
+    message: "Un artículo mensual que resalta los avances en la industria de las telecomunicaciones y cómo impactan a los usuarios.",
+    estrellas: 5,
     foto:"/images/user-4.webp",
-    rol: "Empresario local",
   },
   
   
